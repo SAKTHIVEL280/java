854a4bbd809d31b12d9fcc91b26b50696a4b93dc
SAKTHIVEL E <148361514+SAKTHIVEL280@users.noreply.github.com>
2025-07-29 15:33:11 +0530

    Create operator.java

---
+++ day2/operator.java
@@ -0,0 +1,23 @@
+public class operator {
+    public static void main(String [] args)
+    {
+        /*int n = 8;
+        int m = 9;
+
+        boolean res = n > m; // it will store whether it is true or false for this case it is false
+
+        if (n > m) {
+            System.out.println("n is greater");
+        }
+        else{
+            System.out.println("m is greater");
+        }
+    
+        System.out.println(res);*/
+
+        int n = 8;
+        double m = 8.8;
+        boolean res = n < m; // the result will be true 
+        System.out.println(res);
+    }
+}
