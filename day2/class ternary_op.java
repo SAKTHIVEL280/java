c7f36bf8138a5e8631d11ffb909362bdbd1a1a3f
SAKTHIVEL E <148361514+SAKTHIVEL280@users.noreply.github.com>
2025-07-29 15:45:37 +0530

    Create class ternary_op.java

---
+++ day2/class ternary_op.java
@@ -0,0 +1,10 @@
+public class ternary_op {
+    public static void main(String[]args)
+    {
+        int n = 8;
+        String m;
+
+        m = n>6?"yes":"no"; // ternary operator simple form of assigning or initialize instead of using if else
+        System.out.println(m);
+    }
+}
