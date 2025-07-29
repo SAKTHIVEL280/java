ae86aca4b6372c8641698b6e68bf4977b79f2f1f
SAKTHIVEL E <148361514+SAKTHIVEL280@users.noreply.github.com>
2025-07-29 17:24:33 +0530

    Create forloop.java

---
+++ day2/forloop.java
@@ -0,0 +1,12 @@
+public class forloop {
+    public static void main(String[]args)
+    {
+        for(int i = 1;i<=7;i++)
+        {
+            for(int j = 0;j<=24;j++)
+            {
+                System.out.println("Day : "+i+"     Hour : "+j+":00");
+            }
+        }
+    }
+}
